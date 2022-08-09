8a93afb0b9a9cefedc898052bc9c62cf762153f1
sagarchandresha <sagar.bsquaresoft@gmail.com>
2022-08-09 11:37:58 +0530

    add start script in package file

--- web/index.js
+++ web/index.js
@@ -20,8 +20,8 @@ const PORT = parseInt(process.env.BACKEND_PORT || process.env.PORT, 10);
 // TODO: There should be provided by env vars
 // const DEV_INDEX_PATH = `${process.cwd()}/frontend/`;
 // const PROD_INDEX_PATH = `${process.cwd()}/frontend/dist/`;
-const DEV_INDEX_PATH = `/frontend/`;
-const PROD_INDEX_PATH = `/frontend/dist/`;
+const DEV_INDEX_PATH = `${process.env.HOST.split("://")[0]}/frontend/`;
+const PROD_INDEX_PATH = `${process.env.HOST.split("://")[0]}/frontend/dist/`;
 
 const DB_PATH = `${process.cwd()}/database.sqlite`;
 console.log(DB_PATH)
