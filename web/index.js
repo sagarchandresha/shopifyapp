76131a2a222fa609270bd46d4a0e224ca4d155bf
sagarchandresha <sagar.bsquaresoft@gmail.com>
2022-08-09 12:49:17 +0530

    change index file

--- web/index.js
+++ web/index.js
@@ -18,8 +18,10 @@ const TOP_LEVEL_OAUTH_COOKIE = "shopify_top_level_oauth";
 const PORT = parseInt(process.env.BACKEND_PORT || process.env.PORT, 10);
 
 // TODO: There should be provided by env vars
-const DEV_INDEX_PATH = `${process.cwd()}/frontend/`;
-const PROD_INDEX_PATH = `${process.cwd()}/frontend/dist/`;
+// const DEV_INDEX_PATH = `${process.cwd()}/frontend/`;
+// const PROD_INDEX_PATH = `${process.cwd()}/frontend/dist/`;
+const DEV_INDEX_PATH = `${process.env.HOST}/web/frontend/`;
+const PROD_INDEX_PATH = `${process.env.HOST}/web/frontend/dist/`;
 
 const DB_PATH = `${process.cwd()}/database.sqlite`;
 console.log(DB_PATH)
